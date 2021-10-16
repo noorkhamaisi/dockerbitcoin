09aec4bc20fdcefc05d271d6122d5d430f9a7312
noorkhamaisi <khamaisenoor@gmail.com>
2021-10-16 10:21:13 +0300

    firstocommit

--- main.py
+++ main.py
@@ -1,28 +1,58 @@
 
 from flask import Flask
 import requests
+import time
 
 
 
-  
+def calculateavg(bitcoin_history):
+    summ=0
+    for price in bitcoin_history:
+        price= price.replace(',', '')
+        #print(float(price))
+        summ = summ+float(price)
+        
+    return summ/10
+
+def get_latest_bitcoin_price():
+  response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+  data = response.json()
+  return (data["bpi"]["USD"]["rate"])
+ 
+
+def main():
+    bitcoin_history = []
+    while True:
+        price = get_latest_bitcoin_price()
+        bitcoin_history.append(price)
+
+       
+
+        
+        if len(bitcoin_history) == 10:
+            avg = calculateavg(bitcoin_history)
+            return avg
+
+        # Sleep for 1 minutes 
+        time.sleep(1 * 60)
     
   
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-  data = response.json()
-  print(data["bpi"]["USD"]["rate"])
-  return (data["bpi"]["USD"]["rate"])
+
+  avg10=main()
+  print(get_latest_bitcoin_price())
+  return ("The average of the 10 minutes is: " + str(avg10) + "\nThe current Price is :"+ str(get_latest_bitcoin_price()) )
 
 
 
 
         
-app.run(host="0.0.0.0")   
-#if __name__ == "__main__":
- #   app.run(host="127.0.0.1", port=8087)
+ 
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8989)
 
 
 
--- untitled1.py
+++ untitled1.py
@@ -1,13 +1,42 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 15 11:23:43 2021
 
-@author: get-a
-"""
-
-
+from flask import Flask
 import requests
-response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-data = response.json()
-print(data["bpi"]["USD"]["rate"])
+import time
+from datetime import datetime
 
+def calculateavg(bitcoin_history):
+    summ=0.0
+    for price in bitcoin_history:
+        price= price.replace(',', '')
+       #print(float(price))
+        summ = summ+float(price)
+    return summ/10
+
+def get_latest_bitcoin_price():
+  response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+  data = response.json()
+  mynum=(data["bpi"]["USD"]["rate"])
+ 
+  return (mynum)
+ 
+
+def main():
+    bitcoin_history = []
+    while True:
+        price = get_latest_bitcoin_price()
+        bitcoin_history.append(price)
+
+       
+
+        
+        if len(bitcoin_history) == 3:
+            avg = calculateavg(bitcoin_history)
+            return avg
+
+        # Sleep for 1 minutes 
+       # time.sleep(1 * 60)
+    
+    
+    
+print (main())
+print(get_latest_bitcoin_price())
